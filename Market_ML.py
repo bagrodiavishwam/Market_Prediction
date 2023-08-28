5c704a3055347aa92437e8c510e54edf16609dcf
bagrodiavishwam <98736250+bagrodiavishwam@users.noreply.github.com>
2023-08-28 14:51:21 +0530

    Update Market_ML.py
    Adding ML model, and splitting data into testing and training

--- Market_ML.py
+++ Market_ML.py
@@ -16,3 +16,15 @@ snsx["Tomorrow"] = snsx["Close"].shift(-1)
 
 snsx["Target"] = (snsx["Tomorrow"] > snsx["Close"]).astype(int)
 # snsx = snsx.loc["1990-01-01":].copy()
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
+
+train = snsx.iloc[:-100]
+test = snsx.iloc[-100:]
+
+predictors = ["Close", "Volume", "Open", "High", "Low"]
+model.fit(train[predictors], train["Target"])
+
+
