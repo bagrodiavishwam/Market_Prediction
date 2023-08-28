a6398a74e55eb208700d4acf5f74fcf7ecc4e2d3
bagrodiavishwam <98736250+bagrodiavishwam@users.noreply.github.com>
2023-08-28 14:48:30 +0530

    Update Market_ML.py

--- Market_ML.py
+++ Market_ML.py
@@ -1,3 +1,11 @@
 import yfinance as yf
 import pandas as pd
 import os
+
+if os.path.exists("snsx.csv"):
+    snsx = pd.read_csv("snsx.csv", index_col=0)
+else:
+    snsx = yf.Ticker("^NSEI")
+    snsx = snsx.history(period="max")
+    snsx.to_csv("snsx.csv")
+
