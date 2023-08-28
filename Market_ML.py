13f4d239701f6b000ac1d8b0f4b71f0f266a8381
bagrodiavishwam <98736250+bagrodiavishwam@users.noreply.github.com>
2023-08-28 14:50:28 +0530

    Update Market_ML.py

--- Market_ML.py
+++ Market_ML.py
@@ -10,4 +10,9 @@ else:
     snsx.to_csv("snsx.csv")
 
 del snsx ["Dividends"] 
-del snsx["Stock Splits"]
+del snsx["Stock Splits
+
+snsx["Tomorrow"] = snsx["Close"].shift(-1)
+
+snsx["Target"] = (snsx["Tomorrow"] > snsx["Close"]).astype(int)
+# snsx = snsx.loc["1990-01-01":].copy()
